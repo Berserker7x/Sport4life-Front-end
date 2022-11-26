fab25486b05e321ed776578c32c5b16d382b46f7
ayoub el assari <elassari.ayoub0@gmail.com>
2022-11-26 18:31:05 +0100

    interacting with post

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Components/Login';
+import {Route,Routes,BrowserRouter} from 'react-router-dom'
+import Signup from './Components/Signup'
+import Home from './Components/SideBarComponent/Home'
+import Ask from './Components/SideBarComponent/Ask'
+import FindAthlete from './Components/SideBarComponent/FindAthlete'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+    <Route path="/SignUp" element={<Signup/>} > SignUp </Route>
+    <Route path="/Ask" element={<Ask/>} > Ask </Route>
+    <Route path="/" element={<Login/>} > Login </Route>
+    <Route path="/Home" element={<Home/>} >Home</Route>
+    <Route path="/FindAthlete" element={<FindAthlete/>} >FindAthlete</Route>
+
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
